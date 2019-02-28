243db7cb57188d6bc57e558a927cadb27a78f07f
Renato Campos <renatoineeve@gmail.com>
2019-02-28 20:31:01 +0000

    config

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 class Config:
-    def __init__(self, R):
-        self.R = R
+    def __init__(self, nPhotos):
+        self.nPhotos = nPhotos
     def setData(self, data):
-        self.data = data    
+        self.data = data
+
+    def getOrientation(self, photoIdx):
+        return self.data[photoIdx][0]
+
+    def getTags(self, photoIdx):
+        return self.data[photoIdx][2:]
